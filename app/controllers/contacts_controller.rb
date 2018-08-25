bc206f89495ee54c3fafe4482b71116e6de11f2e
RUKUNDO Jean Claude <rukundo3@gmail.com>
2018-08-25 23:52:57 +0000

    fff

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,49 +1,61 @@
 class ContactsController < ApplicationController
+    before_action :set_contact, only: [:show, :edit, :update, :destroy] 
   
-
-  
-  
-  def new
-   @contact = Contact.new
+  def index
+    @contacts = Contact.all
   end
-   #alidates_length_of :contact_content, :minimum => 1, :maximum => 140, :allow_blank => true
-   #class Contact.new<ActiveRecord
-   #attr_accessible:content
-   #@contact div class "contact_content" validates :title, :content, length:{ in: 1..140 }  
- 
+
+  # Add
+  def new
+  @contacts = Contact.new
+  end
   def create
-    @contact = Contact.new(contact_params)
-    
-    if @contact.save 
-      redirect_to new_contact_path, notice: "You have created new contact!"
+    @contacts = Contact.new(contact_params)
+    if @contacts.save
+      # Switch to the list screen and display a message saying "You have created new blog!"
+      redirect_to contacts_path, Notice: "You have created new contact!"
     else
+      # Redraw the input form.
       render 'new'
     end
-    
-  #   Contact.create(contact_params)
-  #   def contact_params
-  # params.require(:contact).permit(:name, :email, :content)
-  #   end
-     
-    #if @contact.save
-      # Switch to the list screen and display a message saying "You have created new blog!"
-     # redirect_to contacts_path, notice: "You have created new contact!"
-    #else
-      # Redraw the input form.
-      #render 'new'
-    #end
-    # redirect_to new_contact_path
   end
- 
-  #def contact_params
-   # Contact.create(params.require(:contact).permit(:name, :email, :content))
-    #redirect_to new_contact_path
-  #end
-  
-  private
-    def contact_params
-      params.require(:contact).permit(:name,:email,:content)
+  def show
+    # 追記する
+     @contact = Contact.find(params[:id])
+  end
+  def edit
+     @contacts = Contact.find(params[:id])
+  end
+    
+   def update
+    @contacts = Contact.find(params[:id])
+    if @contacts.update(contact_params)
+      redirect_to contacts_path, Notice: "You have edited this duff！"
+    else
+      render 'edit'
     end
-end  
+  end
+  
+  def destroy
+    @contacts.destroy
+    redirect_to contacts_path, Notice: "You have deleted the duff!"
+  end
+  
+  
 
-
+  #Omitted
+  private
+ 
+   def contact_params
+    params.require(:contact).permit(:name, :email, :content)
+   end
+  
+  def set_contact
+    @contacts = Contact.find(params[:id])
+  end
+  
+  #Omission
+  
+  #Omission
+  #Omitted
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221085443) do
+ActiveRecord::Schema.define(version: 20180825224526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
+    t.text "content"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.text "email"
     t.text "content"
   end
 
